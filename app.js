95bfd7750f58f3f5a2450c0817012cb1ff89ca8b
Somya Nagaria <somyainfo18@gmail.com>
2021-06-22 17:50:37 -0500

    final code

--- app.js
+++ app.js
@@ -1,3 +1,70 @@
-const a =  "" 
+        const toDoInputEl= document.querySelector(".todo__input");
+        const toDoListEl = document.querySelector(".todo__list");
+        const toDoItemEls = document.querySelectorAll(".todo__item");
+        
 
-/*ujjujujjjj*/
+
+        function addlistitem(){
+            toDoInputEl .addEventListener("keypress", function(event){
+                if(event.keyCode === 13){
+                    let newListitem = createNewList(toDoInputEl.value)
+                    toDoListEl.appendChild(newListitem);
+                    toDoInputEl.value= "";
+
+                    
+                    
+                }
+
+            
+
+            })
+
+
+        }
+
+        function createNewList(text){
+            const newListitem = document.createElement("li");
+            newListitem.textContent = text;
+            newListitem.setAttribute("class" , "todo__item")
+
+            return newListitem;
+            
+
+
+        }
+        
+        // this is the static code static 
+
+        /*
+        function toggledone(){
+            for(let elms of toDoItemEls ){
+                elms.addEventListener("click", function(){
+                    elms.classList.toggle("done")
+                })
+            }
+
+
+        }
+        */
+        
+        // make it dynamic code
+
+        function toggledone(){
+            toDoListEl.addEventListener("click", function(event){
+               // console.log(event.target.parentNode.children.classList)
+              //  console.log(event.target.parentNode.classList)
+                if(event.target.classList.contains("todo__item")) {
+                 event.target.classList.toggle("done")
+               }
+            })
+       }
+
+        
+        addlistitem();
+
+        createNewList();
+        toggledone();
+
+
+
+
